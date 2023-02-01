fe6324bc29694e235eff23cc22dbcd6a497d1c82
fastbeetech <hezekiahfasanya@gmail.com>
2023-02-01 18:28:03 +0100

    header

--- src/App.js
+++ src/App.js
@@ -2,60 +2,55 @@ import React, { useState } from 'react';
 import Portfolio from './component/Index';
 import Container from './component/portfolio/Container.jsx';
 import AnimatedCursor from 'react-animated-cursor';
-import { MdLightMode, MdOutlineDarkMode } from 'react-icons/md';
+import { MdLightMode, MdDarkMode } from 'react-icons/md';
 
 
 function App() {
   const [darkTheme, setDarkTheme] = useState(false);
   const toggleTheme = () => {
-    // const options = { true: 'dark', false: 'light' };
     setDarkTheme((theme) => !theme);
   };
   return (
-    <Container
-      className={`${darkTheme && 'dark'} dark:bg-gray-800 mont-montserrat`}
-    >
-      <AnimatedCursor
-        innerSize={12}
-        outerSize={30}
-        color="193, 11, 111"
-        outerAlpha={0.2}
-        innerScale={0.7}
-        outerScale={2}
-        clickables={[
-          'a',
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          'input[type="image"]',
-          'label[for]',
-          'select',
-          'textarea',
-          'button',
-          'li',
-          'Link',
-          '.link',
-        ]}
-      />
-      {/* <div className={'absolute sm:-top-2 md:top-3 right-5 z-50'}> */}
-      <div
-        className={'fixed sm:-top-2 md:top-3 sm:right-2 md:right-[2rem] z-50'}
-      >
-        {darkTheme ? (
-          <MdLightMode
-            className="sm:text-[1.75rem] md:text-[2rem] sm:mt-16 md:mt-10 dark:text-gray-100"
-            onClick={() => toggleTheme()}
-          />
-        ) : (
-          <MdOutlineDarkMode
-            className="sm:text-[1.75rem] md:text-[2rem] sm:mt-16 md:mt-10 dark:text-gray-100"
-            onClick={() => toggleTheme()}
-          />
-        )}
-      </div>
-      <Portfolio />
-    </Container>
+      <Container className={`${darkTheme && 'dark'} dark:bg-gray-800 mont-montserrat`}>
+        <AnimatedCursor
+          innerSize={12}
+          outerSize={30}
+          color="193, 11, 111"
+          outerAlpha={0.2}
+          innerScale={0.7}
+          outerScale={2}
+          clickables={[
+            'a',
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            'label[for]',
+            'select',
+            'textarea',
+            'button',
+            'li',
+            'Link',
+            '.link',
+          ]}
+        />
+        {/* <div className={'absolute sm:-top-2 md:top-3 right-5 z-50'}> */}
+        <div className={'fixed sm:top-[12rem] md:top-3 sm:right-2 md:right-[2rem] z-50'}>
+          {darkTheme ? (
+            <MdLightMode
+              className="sm:text-[2rem] md:text-[2rem] dark:text-gray-100"
+              onClick={() => toggleTheme()}
+            />
+          ) : (
+            <MdDarkMode
+              className="sm:text-[2rem] md:text-[2rem] text-gray-800 dark:text-gray-100"
+              onClick={() => toggleTheme()}
+            />
+          )}
+        </div>
+        <Portfolio />
+      </Container>
   );
 }
 
--- src/component/Index.jsx
+++ src/component/Index.jsx
@@ -22,112 +22,103 @@ const menus = [
 ];
 
 const Index = () => {
-
-  //   function handleSetInactive(e) {
-  //     console.log(e);
-  //   }
-  //   function handleSetActive(e) {
-  //     console.log(e);
-  //   }
   return (
-    <div
-      className={` dark:bg-gray-800 dark:text-gray-100 bg-gray-50/50 min-h-full font-montserrat`}
-    >
-      <header className="dark:bg-gray-800 fixed top-0 w-full z-10">
-        <nav className="navbar z-10 dark:text-gray-300 dark:bg-gray-900">
-          {menus.map((menu, index) => (
-            <div key={index}>
-              {menu.id !== 5 && (
-                <Link
-                  key={index}
-                  to={menu.to}
-                  activeClass="active"
-                  spy={true}
-                  smooth={true}
-                  offset={50}
-                  duration={500}
-                  //   onSetActive={handleSetActive}
-                  //   onSetInactive={handleSetInactive}
-                  ignoreCancelEvents={false}
-                  spyThrottle={500}
-                  className="sm:text-[14px] md:text-xl cursor-pointer"
-                >
-                  {menu.text}
-                </Link>
-              )}
-              {menu.id === 5 && (
-                <a
-                  href={menu.to}
-                  className="ssm:text-[14px] md:text-xl cursor-pointe"
-                  target={'_top'}
-                >
-                  {menu.text}
-                </a>
-              )}
-            </div>
-          ))}
-        </nav>
-      </header>
-      <main className="">
-        <div
-          name="home"
-          className="relative lg:w-[65%] lg:mx-auto mt-[5rem] p-5"
-        >
-          <img src={Planet} alt="" className="sm:w-[3.5rem] ml-auto" />
-          <img src={Moon} alt="" className="left-5 sm:w-[3rem]" />
-          <h2 className="mt-[2rem] sm:p-3 sm:text-center sm:text-2xl lg:text-3xl lg:font-semibold">
-            Abiola is a highly competent full-stack web developer with a lot of
-            versatility, flexibility, and a passion for technology and the
-            ability to make ideas come true.
-          </h2>
-          <img src={Rocket} alt="" className="mt-[2rem] sm:w-[4rem] mx-auto" />
-        </div>
-        <div
-          name="aboutme"
-          className="sm:text-center md:text-left mt-5 py-5 dark:bg-gray-700 dark:text-gray-100 bg-white"
-        >
-          <h2 className="px-14 py-5 text-2xl font-semibold ml-2">About me</h2>
-          <About />
-        </div>
-        <div
-          name="skills"
-          className="px-14 sm:text-center md:text-left dark:bg-gray-800 dark:text-gray-100 bg-white"
-        >
-          <h2 className="px-14 py-5 text-2xl font-semibold ml-2">Skills</h2>
-          <Skills />
-        </div>
-        <div name="project" className="mt-5 py-2 dark:bg-gray-700">
-          <h2 className="dark:text-gray-100 dar sm:text-center md:text-left px-14 text-2xl my-2 font-semibold ml-2">
-            Featured Projects
-          </h2>
-          <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            {Projects.map((project, id) => (
-              <div
-                key={id}
-                className={`${project.color} p-5 transition-all ease-in-out delay-200`}
-              >
-                <img src={project.image} alt={project.title} />
-                <div className="flex justify-between items-center">
-                  <a href={project.link} className={`py-3 px-5 rounded-sm`}>
-                    View Project
+      <div
+        className={` dark:bg-gray-800 dark:text-gray-100 bg-gray-50/50 min-h-full font-montserrat`}
+      >
+        <header className="dark:bg-gray-800 fixed top-0 w-full z-10">
+          <nav className={'navbar dark:text-gray-300 dark:bg-gray-900'}>
+            {menus.map((menu, index) => (
+              <div key={index}>
+                {menu.id !== 5 && (
+                  <Link
+                    key={index}
+                    to={menu.to}
+                    activeClass="active"
+                    spy={true}
+                    smooth={true}
+                    offset={50}
+                    duration={500}
+                    ignoreCancelEvents={false}
+                    spyThrottle={500}
+                    className="sm:text-[14px] md:text-xl cursor-pointer"
+                  >
+                    {menu.text}
+                  </Link>
+                )}
+                {menu.id === 5 && (
+                  <a
+                    href={menu.to}
+                    className="ssm:text-[14px] md:text-xl cursor-pointe"
+                    target={'_top'}
+                  >
+                    {menu.text}
                   </a>
-                  <a href={project?.github} className={` py-3 px-5 rounded-sm`}>
-                    Github
-                  </a>
-                </div>
+                )}
               </div>
             ))}
+          </nav>
+        </header>
+        <main className="">
+          <div
+            name="home"
+            className="relative lg:w-[65%] lg:mx-auto my-[5rem] p-5"
+          >
+            <img src={Planet} alt="" className="sm:w-[3.5rem] ml-auto" />
+            <img src={Moon} alt="" className="left-5 sm:w-[3rem]" />
+            <h2 className="mt-[2rem] sm:p-3 sm:text-center sm:text-2xl lg:text-3xl lg:font-semibold">
+              Abiola is a highly competent full-stack web developer with a lot of
+              versatility, flexibility, and a passion for technology and the
+              ability to make ideas come true.
+            </h2>
+            <img src={Rocket} alt="" className="mt-[2rem] sm:w-[4rem] mx-auto" />
           </div>
-        </div>
-        <div name="contact" className="pt-5">
-          <h2 className="dark:text-gray-100 sm:text-center md:text-left px-14 text-2xl my-2 font-semibold ml-2">
-            Contact
-          </h2>
-          <Contact />
-        </div>
-      </main>
-      <Footer />
-    </div>
+          <div
+            name="aboutme"
+            className="sm:text-center md:text-left mt-5 py-5 dark:bg-gray-700 dark:text-gray-100 bg-white"
+          >
+            <h2 className="px-14 py-5 text-2xl font-semibold ml-2">About me</h2>
+            <About />
+          </div>
+          <div
+            name="skills"
+            className="px-14 sm:text-center md:text-left dark:bg-gray-800 dark:text-gray-100 bg-white"
+          >
+            <h2 className="px-14 py-5 text-2xl font-semibold ml-2">Skills</h2>
+            <Skills />
+          </div>
+          <div name="project" className="mt-5 py-2 dark:bg-gray-700">
+            <h2 className="dark:text-gray-100 dar sm:text-center md:text-left px-14 text-2xl my-2 font-semibold ml-2">
+              Featured Projects
+            </h2>
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+              {Projects.map((project, id) => (
+                <div
+                  key={id}
+                  className={`${project.color} p-5 transition-all ease-in-out delay-200`}
+                >
+                  <img src={project.image} alt={project.title} />
+                  <div className="flex justify-between items-center">
+                    <a href={project.link} className={`py-3 px-5 rounded-sm`}>
+                      View Project
+                    </a>
+                    <a href={project?.github} className={` py-3 px-5 rounded-sm`}>
+                      Github
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div name="contact" className="pt-5">
+            <h2 className="dark:text-gray-100 sm:text-center md:text-left px-14 text-2xl my-2 font-semibold ml-2">
+              Contact
+            </h2>
+            <Contact />
+          </div>
+        </main>
+        <Footer />
+      </div>
   );
 };
 
