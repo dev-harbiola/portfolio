9d24302f6ebf9176b33c20a31ed5496a1e9bb1ae
fastbeetech <hezekiahfasanya@gmail.com>
2023-02-03 14:12:50 +0100

    Icon update for menu

--- src/component/data/menus.jsx
+++ src/component/data/menus.jsx
@@ -1,11 +1,11 @@
-import { BiHomeAlt, BiBook, BiBriefcaseAlt, BiMessageSquareDetail} from 'react-icons/bi';
+import { BiHomeAlt, BiUser, BiBriefcaseAlt2, BiMessage} from 'react-icons/bi';
 
 class Menu {
   static menus = [
     { id: 1, to: 'home', text: 'Home', icon: BiHomeAlt },
-    { id: 2, to: 'aboutme', text: 'About', icon: BiBook },
-    { id: 3, to: 'project', text: 'Project', icon: BiBriefcaseAlt },
-    { id: 4, to: 'contact', text: 'Contact', icon: BiMessageSquareDetail },
+    { id: 2, to: 'aboutme', text: 'About', icon: BiUser },
+    { id: 3, to: 'project', text: 'Project', icon: BiBriefcaseAlt2 },
+    { id: 4, to: 'contact', text: 'Contact', icon: BiMessage },
   ];
 }
 
