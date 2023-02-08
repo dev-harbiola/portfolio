94a553f85efc0cb4ec2f70c18dc8884ed559db1c
fastbeetech <hezekiahfasanya@gmail.com>
2023-02-08 19:39:52 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -8,30 +8,6 @@ function App() {
   const { theme } = useTheme();
   return (
     <Container className={`${theme && 'dark'} dark:bg-gray-800 font-poppins`}>
-      {/* <AnimatedCursor
-        innerSize={12}
-        outerSize={30}
-        color="193, 11, 111"
-        outerAlpha={0.2}
-        innerScale={0.7}
-        outerScale={2}
-        clickables={[
-          'a',
-          'input[type="text"]',
-          'input[type="email"]',
-          'input[type="number"]',
-          'input[type="submit"]',
-          'input[type="image"]',
-          'label[for]',
-          'select',
-          'textarea',
-          'button',
-          'li',
-          'Link',
-          '.link',
-        ]}
-      /> */}
-
       <Portfolio />
     </Container>
   );
