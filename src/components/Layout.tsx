a64d37805ef3971ddbdae0eb4f16fb43c2994780
abiolafasanya <hezekiahfasanya@gmail.com>
2023-07-29 23:35:41 +0100

    refactor: removed unused code

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,37 +1,24 @@
 import { Outlet } from "react-router-dom";
-import useApp from "../hooks/useApp";
-import clsx from "clsx";
 import React, { useEffect, useState } from "react";
 import Header from "../ui/header/Header";
 import Footer from "../ui/footer/Footer";
 import Loader from "../ui/loader/Loader";
 
-interface Props {
-  variant?: "light" | "dark" | "outline" | "plain";
-}
-
-const Layout = ({ variant }: Props) => {
-  const { dark } = useApp();
-  const [theme, setTheme] = useState<typeof variant>();
+const Layout = () => {
   const [isLoading, setIsloading] = useState(true);
   useEffect(() => {
     setIsloading(true);
     setTimeout(() => {
       setIsloading(false);
     }, 3000);
-    const current = true ? "dark" : "light";
-    setTheme(current);
-  }, [dark]);
-  const styles = clsx("w-full relative max-w-[1920px] font-montserrat", {
-    "bg-zinc-100 text-black": theme === "light",
-    "bg-zinc-900 text-zinc-100": theme === "dark",
-  });
+  }, []);
+
   return (
     <React.Fragment>
       {isLoading ? (
         <Loader />
       ) : (
-        <div className={styles}>
+        <div className="w-full relative max-w-[1920px] font-montserrat bg-zinc-900 text-zinc-100">
           <div className="fixed top-0 z-50 w-full bg-zinc-900">
             <Header />
           </div>
